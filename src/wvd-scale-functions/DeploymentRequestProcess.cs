00e17d4054f2745f26aa23dccbfa3564fc1828bf
Mariusz Kolodziej <Mariusz.Kolodziej@microsoft.com>
2021-03-15 21:20:25 -0400

    updated with ServiceBus message push

--- src/wvd-scale-functions/DeploymentRequestProcess.cs
+++ src/wvd-scale-functions/DeploymentRequestProcess.cs
@@ -6,12 +6,17 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Azure.Messaging.ServiceBus;
 
 namespace MK.WVD
 {
     public static class DeploymentRequestProcess
     {
+        //static string connectionString = "";
+        static string queueName = "wvd-request-session";
+
         [FunctionName("DeploymentRequestProcess")]
+        //[return: ServiceBus("myqueue", Connection = "ServiceBusConnection")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
@@ -27,6 +32,20 @@ namespace MK.WVD
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
                 : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+            await using (ServiceBusClient client = new ServiceBusClient(System.Environment.GetEnvironmentVariable("ServiceBusConnection")))
+            {
+                // create a sender for the queue
+                ServiceBusSender sender = client.CreateSender(queueName);
+
+                // create a message that we can send
+                ServiceBusMessage message = new ServiceBusMessage("Hello world!");
+                //message.MessageId = "supermario";
+                message.SessionId = "1";
+
+                // send the message
+                await sender.SendMessageAsync(message);
+            }
 
             return new OkObjectResult(responseMessage);
         }
